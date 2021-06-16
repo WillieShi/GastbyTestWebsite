dec4984c362e69ad184b0d9c9acf6deea0b90397
shiguy <williamlongshi@gmail.com>
2021-06-16 17:46:59 -0500

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: "Hello World Site",
+    title: "Hello World Site, Queried Title",
   },
   plugins: [
   "gatsby-plugin-gatsby-cloud",
   "gatsby-plugin-image",
-  "gatsby-plugin-sharp",],
+  "gatsby-plugin-sharp",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  },
+],
 };
